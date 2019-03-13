4b8a7a99c85717dbfad1997b307c4602cfb99ca6
funcake <1078715589@qq.com>
2019-03-13 18:55:12 +0800

    'ok'

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="referer" content="never">
+	<title>hello</title>
+</head>
+<body>
+	<script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" charset="utf-8">
+</script>
+
+@foreach($list as $user)
+	<div class="list">
+	<img src="{{$user['product_base']['img'][0]}}" alt="">
+	<span>{{$user['product_base']['name']}}</span><a href="express" title="发货">发货</a>
+	</div>
+@endforeach
+
+</form>
+</body>
+</html>
