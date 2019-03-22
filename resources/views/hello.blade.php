96df0154f3b5d32866b1c82564607ec4c2f8600e
funcake <1078715589@qq.com>
2019-03-22 18:53:24 +0800

    'sdf'

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -2,14 +2,14 @@
 @section("content")
 @foreach($list as $user)
 <div class="row">
-	<div class="col-sm-3 col-xs-3">
-		<img src="{{$user['headimgurl']}}" alt="">
+	<div class="col-sm-3 col-3">
+		<img src="{{$user['headimgurl']}}" alt="" class="img-responsive">
 	</div>
-	<div class="col-sm-3 col-xs-3">
+	<div class="col-sm-3 col-3">
 		<span >{{$user['nickname']}}</span>
 	</div>
-	<div class="col-sm-3 col-xs-3"></div>
-	<div class="col-sm-3 col-xs-3"><a href="express" title="发货" class="btn btn-primary">发货</a></div>
+	<div class="col-sm-3 col-3"></div>
+	<div class="col-sm-3 col-3"><a href="express" title="发货" class="btn btn-primary">发货</a></div>
 </div>
 <hr>
 @endforeach
