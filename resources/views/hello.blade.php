af369e300f0b908178cbaa3824a4273da6d80d0f
funcake <1078715589@qq.com>
2019-03-22 18:55:21 +0800

    'sdf'

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -5,10 +5,10 @@
 	<div class="col-sm-3 col-3">
 		<img src="{{$user['headimgurl']}}" alt="" class="img-responsive">
 	</div>
+	<div class="col-sm-3 col-3"></div>
 	<div class="col-sm-3 col-3">
 		<span >{{$user['nickname']}}</span>
 	</div>
-	<div class="col-sm-3 col-3"></div>
 	<div class="col-sm-3 col-3"><a href="express" title="发货" class="btn btn-primary">发货</a></div>
 </div>
 <hr>
