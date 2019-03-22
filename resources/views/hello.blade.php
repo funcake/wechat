ebd4559bc1b6e4dc766b2e86b63d38a3013e53b9
funcake <1078715589@qq.com>
2019-03-22 18:43:21 +0800

    'sdf'

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -2,14 +2,14 @@
 @section("content")
 @foreach($list as $user)
 <div class="row">
-	<div class="col-sm-3">
+	<div class="col-sm-3 col-xs-3">
 		<img src="{{$user['headimgurl']}}" alt="">
 	</div>
-	<div class="col-sm-3">
-		<span class="text-success">{{$user['nickname']}}</span>
+	<div class="col-sm-3 col-xs-3">
+		<span >{{$user['nickname']}}</span>
 	</div>
-	<div class="col-sm-3"><p class="btn btn-default">saldkfjslkdfj</p></div>
-	<div class="col-sm-3"><p class="bg-danger"><a href="express" title="发货" class="btn btn-default">发货</a></p></div>
+	<div class="col-sm-3 col-xs-3"></div>
+	<div class="col-sm-3 col-xs-3"><a href="express" title="发货" class="btn btn-primary">发货</a></div>
 </div>
 <hr>
 @endforeach
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 	<title> </title>
 	<meta  charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap-grid.min.css" rel="stylesheet">
+<link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js" type="text/javascript" charset="utf-8"></script>
