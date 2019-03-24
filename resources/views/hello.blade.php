2e543e0c0c9df6584fa3b5326e3b6eaae83a638f
funcake <1078715589@qq.com>
2019-03-24 12:21:51 +0800

    'a'

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -2,11 +2,11 @@
 @section("content")
 @foreach($list as $product)
 <div class="row">
-	<div class="col-sm-3 col-3">
-		<img src="{{chop($product['product_base']['main_img'],'?wx_fmt=jpeg')}}" alt="" class="img-responsive img-thumbnail">
+	<div class="col-sm-4 col-4">
+		<img src="{{chop($product['product_base']['main_img'],'?wx_fmt=jpeg')}}" alt="" class="img-thumbnail">
 	</div>
-	<div class="col-sm-3 col-3"></div>
-	<div class="col-sm-3 col-3">
+	<div class="col-sm-1 col-1"></div>
+	<div class="col-sm-4 col-4">
 		<span >{{$product['product_base']['name']}}</span>
 	</div>
 	<div class="col-sm-3 col-3"><a href="express" title="发货" class="btn btn-primary">发货</a></div>
