ada87e67d0223fa997e8931082dc987490614bc6
funcake <1078715589@qq.com>
2019-03-29 12:36:16 +0800

    'a'

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -18,7 +18,7 @@ class WeChatController extends Controller
     public function serve()
     {
         // Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-$user = session('oauth');
+$user = session('wechat.oauth_user');
 dd($user);
 
         $app = app('wechat.official_account');
