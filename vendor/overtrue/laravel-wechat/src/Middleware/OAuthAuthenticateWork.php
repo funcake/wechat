d24c0cd450176b26c5f8d51d0e58ae21b7ff2e48
funcake <1078715589@qq.com>
2019-04-13 20:39:02 +0800

    'a

--- vendor/overtrue/laravel-wechat/src/Middleware/OAuthAuthenticateWork.php
+++ vendor/overtrue/laravel-wechat/src/Middleware/OAuthAuthenticateWork.php
@@ -60,10 +60,9 @@ if ($session) {
             if ($request->has('code')) {
                 session([$sessionKey => $officialAccount->oauth->user() ?? []]);
                 $isNewSession = true;
-                // dd(session($sessionKey));
 
-                // Event::fire(new WeChatUserAuthorized(session($sessionKey), $isNewSession, $account));
-                return $request;
+                Event::fire(new WeChatUserAuthorized(session($sessionKey), $isNewSession, $account));
+                return redirect()->to($this->getTargetUrl($request));
             }
 
             session()->forget($sessionKey);
