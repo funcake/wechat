c2c7a5f0e5302cc4a8c448cb6524befd7c0e4ae6
funcake <1078715589@qq.com>
2019-04-15 11:13:27 +0800

    'a

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -21,7 +21,7 @@ class WeChatController extends Controller
 // var_dump(session()->all());
 
         $this->middleware('oauth:snsapi_userinfo'); 
-        $this->middleware('work:snsapi_userinfo'); 
+        // $this->middleware('work:snsapi_userinfo'); 
     }
 
     public function home(Request $request) {
