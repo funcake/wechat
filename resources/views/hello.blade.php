237fb5104572f6f8530b8dcf40817956af20d334
funcake <1078715589@qq.com>
2019-04-20 22:07:05 +0800

    'a

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -11,38 +11,21 @@
 	<div data-role="popup" id="popup" data-theme="a" data-overlay-theme="b" class="ui-content" style="max-width:340px; padding-bottom:2em;">
 
 		<form>
-				<ul data-role="listview" data-inset="true">
-					<li class="ui-field-contain">
-			        <label for="name" class="ui-hidden-accessible"></label>
-			            <input type="text" name="name" id="name" value="" data-clear-btn="true" placeholder="名" >
-						
-					</li>
-				<li class="ui-field-contain">
-			        <label for="price" class="ui-hidden-accessible">价:</label>
-			            <input type="range" name="price" id="price" value="" min="0" max="100" step="" data-highlight="true" data-popup-enable="true" >
-			<div class="ui-field-contain">
-					<fieldset data-role="controlgroup" data-type="horizontal" data-mini="true">
-						        <input type="radio" name="pricefield" id="primary" value="1000" checked="checked" onclick="$('#price').attr({'step':20,'max':1000});">
-						        <label for="primary">千元</label>
-						        <input type="radio" name="pricefield" id="advance" value="5000" onclick="	$('#price').attr({'step':100,'min':100,'max':5000});">
-						        <label for="advance">五千</label>
-						        <input type="radio" name="pricefield" id="master" value="10000" onclick="$('#price').attr({'step':200,'min':5000,'max':10000});">
-						        <label for="master">万元</label>
-					</fieldset>
-			</div>	
-					
-				</li>
-				<li class="ui-field-contain">
-					
-			            <label for="material" class="select" data-inline='true'>料：</label>
-						<select id="material" name="material" key="{{$material['id']}}" class='select' data-inline='true' >
-						@foreach($material['property_value'] as $p)
-							<option value="{{$p['id']}}">{{$p['name']}}</option>
-						@endforeach
-						</select>
-				</li>
-
-					<li class="ui-field-contain">
+					<label for="name" class="ui-hidden-accessible"></label><input type="text" name="name" id="name" value="" data-clear-btn="true" placeholder="名" >
+					<br>
+					<select name="level" id = "level" onchange="select(this.value)">
+						<option value="1000" >千元档</option>
+						<option value="5000" >五千档</option>
+						<option value="10000" >万元档</option>
+					</select>
+				    <label for="price" class="ui-hidden-accessible">价:</label><input type="range" name="price" id="price" value="" min="0" max="100" step="" data-highlight="true" data-popup-enable="true" >
+				    <br>
+					<label for="material" class="select" data-inline='true'>料:</label>
+					<select id="material" name="material" key="{{$material['id']}}" class='select' data-inline='true' >
+					@foreach($material['property_value'] as $p)
+						<option value="{{$p['id']}}">{{$p['name']}}</option>
+					@endforeach
+					</select>
 					<fieldset data-role = "controlgroup" data-type="horizontal" >
 						<legend >属性:</legend>
 						<label for="usage" class="select" data-inline='true'>适用场景</label>
@@ -58,56 +41,20 @@
 						@endforeach
 						</select>
 					</fieldset>
-						
-					</li>
-					<li class="ui-body ui-body-a">
-						    <fieldset class="ui-grid-a">
-							<div class="ui-block-a">
-								
-								<a href="" data-rel="back" class="ui-shadow ui-btn  ui-btn-a ui-corner-all   ui-icon-action ui-btn-icon-left"  key="" id="submit">提交</a>
-							</div>
-							<div class="ui-block-b">
-								
-								    <a href="" data-rel="back" class="ui-shadow ui-btn ui-btn-b ui-corner-all ">取消</a>
-							</div>
-						            </fieldset>
-						
-					</li>
-				</ul>
 
+					<br>
+					<fieldset class="ui-grid-a">
+						<div class="ui-block-a"><a href="" data-rel="back" class="ui-shadow ui-btn  ui-btn-a ui-corner-all   ui-icon-action ui-btn-icon-left"  key="" id="submit">提交</a></div>
+						<div class="ui-block-b"><a href="" data-rel="back" class="ui-shadow ui-btn ui-btn-b ui-corner-all ">取消</a></div>
+					</fieldset>
 		</form>
 
-	</div>
 
-	<div data-role="popup" id="popup1" data-theme="a" data-overlay-theme="b" class="ui-content" style="max-width:340px; padding-bottom:2em;">
-		<form method="post">
 
-			        <label for="price" class="ui-hidden-accessible">价:</label>
-			        <input type="range" name="price" id="price" value="" min="0" max="100" step="" data-highlight="true" data-clear-btn="true" >
-
-			<div class="ui-field-contain">
-						<fieldset data-role="controlgroup" data-type="horizontal" data-mini="true">
-							        <input type="radio" name="pricefield" id="primary" value="1000" checked="checked" onclick="$('#price').attr({'step':20,'max':1000});">
-							        <label for="primary">千元</label>
-							        <input type="radio" name="pricefield" id="advance" value="5000" onclick="	$('#price').attr({'step':100,'min':100,'max':5000});">
-							        <label for="advance">五千</label>
-							        <input type="radio" name="pricefield" id="master" value="10000" onclick="$('#price').attr({'step':200,'min':5000,'max':10000});">
-							        <label for="master">万元</label>
-						</fieldset>
-				</div>	
-
-		        <fieldset class="ui-grid-a">
-					<a href="" data-rel="back" class="ui-shadow ui-btn ui-corner-all ui-btn-inline  submit"  key="">提交</a>
-				    <a href="" data-rel="back" class="ui-shadow ui-btn ui-corner-all ui-btn-inline">取消</a>
-	            </fieldset>
-		</form>
-
-	</div>
-
+</section><!-- /page -->
 	<footer data-role="footer">
 		<h1>Footer</h1>
 	</footer><!-- /footer -->
-</section><!-- /page -->
 
 <script type="text/javascript" charset="utf-8" async defer>
 
@@ -120,21 +67,21 @@
 		post.product_base.name=$('#name').val();
 		post.sku_list[0].price=$('#price').val()*100;
 
-		post.product_base.property = [
+		post.product_base.property = 
+		[
 			{
-				"id": {{$material['id']}},
+				"id": '{{$material['id']}}',
 				"vid": $('#material').val(),
 			},
 			{
-				"id": {{$style['id']}},
+				"id": '{{$style['id']}}',
 				"vid": $('#style').val(),
 			},
 			{
-				"id": {{$usage['id']}},
+				"id": '{{$usage['id']}}',
 				"vid": $('#usage').val(),
 			}
 		];		
-		console.log(post);
 
 		$("#"+key+' h2').html($('#name').val());
 		$("#"+key+" p").html('￥'+$('#price').val());
@@ -146,6 +93,26 @@
 		);
 	});
 
+	function select(val) {
+		console.log(val);
+		switch (val) {
+			case '1000':
+				$('#price').attr({'step':20,'min':0,'max':1000});
+				$('#price').val(0);
+				break;
+			case '5000':
+					$('#price').attr({'step':100,'min':1000,'max':5000});
+					$('#price').val(1000);
+					break;
+			case '10000':
+					$('#price').attr({'step':200,'min':5000,'max':10000});
+				$('#price').val(5000);
+			default:
+				break;
+		}
+					
+
+	}
 
 	$.get(
 		'0',
@@ -171,20 +138,22 @@
 				var i = $(this).attr('key');
 				$('#name').val(data[i].product_base.name);
 				$price = data[i].sku_list[0].price/100;
-				if ($price<1000) {
-					$('#primary').click();
-					$('#price').attr({'step':20,'max':1000});
+				if ($price<1000) 
+				{	
+					$('#level').click();
+					$('#level').prev().html('千元档');
 				} else if($price<5000) {
-					$('#advance').click();
-					$('#price').attr({'step':100,'min':100,'max':5000});
-				} else if($price<5000) {
-					$('#master').click();
-					$('#price').attr({'step':200,'min':5000,'max':10000});
+					$('#level').click();
+					$('#level').prev().html('五千档');
+				} else if($price<=10000) {
+					$('#level').click();
+					$('#level').prev().html('万元档');
 				}
 				$('#price').val($price);
-				$('.submit').attr('key',i);
+				$('#submit').attr('key',i);
 			});
 		}
 	);
+
 	</script>
 	@endsection
