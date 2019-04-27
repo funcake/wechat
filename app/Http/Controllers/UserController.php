24173e5458ceeefaf0b7cabe777b60e2da05bb6b
funcake <1078715589@qq.com>
2019-04-27 14:43:45 +0800

    'a\

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function create() {
+        return 123;
+    }
+
+    public function change() {
+        return app('wechat.work.user')->server->serve();
+    }
+}
