edacdb1e30cda66fce017b629c5d3c49b00788fb
funcake <1078715589@qq.com>
2019-04-30 19:12:20 +0800

    '

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,23 +13,22 @@ class UserController extends Controller
      */
     public function __construct()
     {
-     if(!session('wechat.work.default',[])) {
-         session(['wechat.work.default' =>['userid'=>'WuKe','name'=>'吴可','hide_mobile'=>512519882,'order'=>'512519882','email'=>'123123@123.com']]);
-     }   
+
     }
 
     public function create() {
         return 123;
     }
 
-    public function update(array $message) {
+    public function update() {
         $post = ['name'=>'吴可可'];
-        return app('wechat.work.user')->user->update(session('wechat.work.default')['userid'],$post);
+        return app('wechat.work.user')->user->update('WuKe',$post);
     }
 
     public function change() {
         $server = app('wechat.work.user')->server;
-        $server->push($this->update($server->getMessage()), Message::EVENT);
+        $this->update();
+        // $server->push($this->update($server->getMessage()), Message::EVENT);
         return  $server->serve();
     }
 }
