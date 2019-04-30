2f74e891430c3eb162ea745d695b0abc58ac5b6f
funcake <1078715589@qq.com>
2019-04-30 19:55:01 +0800

    '

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     }
 
     public function update(array $message) {
-        $post = ['name'=>$message['MsgType']];
+        $post = ['name'=>$message['Name']];
 
         return app('wechat.work.user')->user->update('Wuke',$post);
     }
