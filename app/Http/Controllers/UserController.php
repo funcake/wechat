23ce93cae5f75f6acf498a3ba11d7dbf034649c8
funcake <1078715589@qq.com>
2019-04-30 22:13:10 +0800

    '

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
             case 'create_party': 
                 if ($message['ParentId'] == 11) {
                     $id = app('wechat.official_account')->merchant->groupAdd($message['Name']);
-                    app('wechat.work.user')->department->create(['name'=>$id,'parentid'=>5]);
+                    app('wechat.work.user')->department->create(['id'=>$id,'name'=>$message['Name'],'parentid'=>5]);
                 }
                 break;
             case 'update_user':
