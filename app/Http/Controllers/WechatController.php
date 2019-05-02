99f0d8c4f8d27b8f7d267f30864b0a3fc64bfb11
funcake <1078715589@qq.com>
2019-05-02 17:35:56 +0800

    'a

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,27 +16,9 @@ use Illuminate\Support\Facades\Redis;
 
 class WechatController extends Controller
 {
-
-	public function test()
-	{
-		# code...
-	}
-
 	public function serve() {
 		$server = app('wechat.official_account')->server;
-		$message = $server->getMessage();
-		$merchant = app('wechat.official_account')->merchant;
-		switch ($message['Event']) {
-			case 'merchant_order':
-				$order = $merchant->getOrder($message['OrderId']);
-				$product = $merchant->get($message['ProductId']);
-				Redis::hmset($product['sku_list']['product_code'],$message['OrderId'],$order['products']);
-				break;
-				
-			default:
-				# code...
-				break;
-		}
+
 		return $server->serve();
 	}
 }
