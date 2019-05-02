aca5e0d01d34eb2d97a575513392ef5aac89a36c
funcake <1078715589@qq.com>
2019-05-02 17:34:34 +0800

    'a

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ $router->post('','ProductController@update');
 
 $router->post('delete','ProductController@delete');
 
-$router->get('wechat','WeChatController@test');
-// $router->post('wechat','WeChatController@serve');
+$router->get('wechat','WechatController@test');
+$router->post('wechat','WechatController@serve');
 
 $router->get('user/create','UserController@create');
 // $router->get('admin','WeChatController@group');
