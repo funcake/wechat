8e466e82be185cfd7a7402660c2c2d99920ea959
funcake <1078715589@qq.com>
2019-05-09 22:02:12 +0800

    '

--- app/Http/Controllers/ServeController.php
+++ app/Http/Controllers/ServeController.php
@@ -57,7 +57,7 @@ class ServeController extends Controller
 		        case 'update_user ':
 		        	if (isset($message['IsLeaderInDept'])) {
 		        		// if(($key = array_search(1, $message['IsLeaderInDept'])) !==false ) {
-			        		$this->dispatch(new RegistUser($message['UserID'],$key));
+			        		$this->dispatch(new RegistUser($message['UserID'],1));
 			        	// }
 		        	}
 		        	break;
