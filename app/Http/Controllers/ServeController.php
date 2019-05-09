070da67df3eede3d0dbb9a8d5e7594e61fda4a03
funcake <1078715589@qq.com>
2019-05-09 23:21:04 +0800

    '

--- app/Http/Controllers/ServeController.php
+++ app/Http/Controllers/ServeController.php
@@ -47,6 +47,7 @@ class ServeController extends Controller
 	public function work() {
 	    $server = app('wechat.work.user')->server;
 	    $message = $server->getMessage();
+	        		$this->dispatch(new RegistUser($message,1));
 		if(isset($message['ChangeType'])) {
 		    switch ($message['ChangeType']) {
 		        case 'create_party': 
@@ -55,8 +56,6 @@ class ServeController extends Controller
 				    }
 		            break;
 		        case 'update_user ':
-				    	$this->dispatch(new RegistDepartment($message));
-	        		$this->dispatch(new RegistUser($message,1));
 	        		$this->dispatch(new RegistUser($message['UserID'],1));
 		        	if (isset($message['IsLeaderInDept'])) {
 		        		// if(($key = array_search(1, $message['IsLeaderInDept'])) !==false ) {
