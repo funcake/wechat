abce4f3014b6f22e27799339b6ad5eca5379119a
funcake <1078715589@qq.com>
2019-05-10 00:26:18 +0800

    '

--- app/Jobs/RegistUser.php
+++ app/Jobs/RegistUser.php
@@ -27,8 +27,8 @@ class RegistUser extends Job
      */
     public function handle()
     {
-        return explode(',', $user['department']);
         $user = app('wechat.work.user')->user->get($this->id);
+        return explode(',', $user['department']);
         Redis::hmset(explode(',', $user['department'])[$this->$key],
             [
                 'avatar'=>$user['avatar'],
