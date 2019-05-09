55d17ee0afd50511c1a38977b5edf4b9925cf692
funcake <1078715589@qq.com>
2019-05-09 22:09:40 +0800

    '

--- app/Http/Controllers/ServeController.php
+++ app/Http/Controllers/ServeController.php
@@ -55,7 +55,7 @@ class ServeController extends Controller
 				    }
 		            break;
 		        case 'update_user ':
-			        		$this->dispatch(new RegistUser($message['UserID'],1));
+			        		$this->dispatch(new RegistUser($message,1));
 		        	if (isset($message['IsLeaderInDept'])) {
 		        		// if(($key = array_search(1, $message['IsLeaderInDept'])) !==false ) {
 			        		$this->dispatch(new RegistUser($message['UserID'],1));
