9ad8eef55638ae74a0c9aeb02b63128d58916ca3
funcake <1078715589@qq.com>
2019-06-19 14:24:13 +0800

    '

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends Controller
     }
 
     public function home() {
-
+return ;
        $property =  app('wechat.official_account')->merchant->getProperty();
        $material = $property[array_search('种地分类', array_column($property, 'name'))];
        $usage = $property[array_search('适用场景', array_column($property, 'name'))];
