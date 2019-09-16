d892a17612378d8fb1cd5b095d864c3c2dbdf1a9
funcake <1078715589@qq.com>
2019-09-16 13:21:17 +0800

    /

--- resources/views/merchant/layout.blade.php
+++ resources/views/merchant/layout.blade.php
@@ -1,9 +1,5 @@
 <!DOCTYPE HTML>
-<!--
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+
 <html>
 	<head>
 		<title>福临璟苑</title>
@@ -161,7 +157,7 @@
 									</article>			
 								` ;
 							}
-							$('#products').html(html);
+							$('#products').append(html);
 					}
 				);
 
